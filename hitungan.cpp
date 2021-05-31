c5e3f83f31852a242b2ff48bfc35c101b2c3135c
ZICO Nakano M <69408905+ZICONakano@users.noreply.github.com>
2021-05-31 12:24:55 +0700

    Add files via upload

---
+++ hitungan.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a = /* masukkan angka */;
+    float b = /* masukkan angka */;
+
+    float hasil; 
+
+     // penjumlahan
+    hasil = a + b;
+    cout << a << " + " << b << " = " << hasil << endl;
+    
+     // pengurangan
+    hasil = a - b;
+    cout << a << " - " << b << " = " << hasil << endl;
+    
+     // perkalian
+    hasil = a * b;
+    cout << a << " * " << b << " = " << hasil << endl;
+    
+     // pembagian
+    hasil = a / b;
+    cout << a << " / " << b << " = " << hasil << endl;
+    
+    cin.get();
+    return 0;
+
+}
